de60cc92e729074b7ca76728cd68f3339a655962
Arkadiusz <karas.tsukasa@gmail.com>
2019-12-20 21:02:52 +0100

    Adding Zalenium Hub and new Capabilities

--- src/main/java/com/demo/project/listeners/DemoProjectListener.java
+++ src/main/java/com/demo/project/listeners/DemoProjectListener.java
@@ -4,6 +4,10 @@ import com.codeborne.selenide.Configuration;
 import com.demo.project.documentation.DocumentedTest;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import io.qameta.allure.Allure;
+import org.openqa.selenium.Platform;
+import org.openqa.selenium.remote.BrowserType;
+import org.openqa.selenium.remote.CapabilityType;
+import org.openqa.selenium.remote.DesiredCapabilities;
 import org.testng.*;
 
 import java.lang.reflect.InvocationTargetException;
@@ -13,9 +17,9 @@ import java.util.Map;
 public class DemoProjectListener implements ISuiteListener, IInvokedMethodListener2 {
     @Override
     public void beforeInvocation(IInvokedMethod method, ITestResult testResult, ITestContext context) {
-       if(method.isTestMethod()) {
-           assignDocumentationToTest(method);
-       }
+        if (method.isTestMethod()) {
+            assignDocumentationToTest(method);
+        }
     }
 
     @Override
@@ -33,15 +37,27 @@ public class DemoProjectListener implements ISuiteListener, IInvokedMethodListen
 
     }
 
+    private DesiredCapabilities firefoxCapabilities() {
+        DesiredCapabilities cap = new DesiredCapabilities();
+        cap.setCapability(CapabilityType.BROWSER_NAME, BrowserType.FIREFOX);
+        cap.setCapability(CapabilityType.PLATFORM_NAME, Platform.LINUX);
+        return cap;
+    }
+
     @Override
     public void onStart(ISuite suite) {
         Configuration.browser = System.getProperty("selenide.browser", "firefox");
+        Configuration.remote = System.getenv("ZALENIUM_HUB");
+
         Map.of("firefox", WebDriverManager.firefoxdriver(),
                 "chrome", WebDriverManager.chromedriver(),
                 "edge", WebDriverManager.edgedriver(),
                 "phantomjs", WebDriverManager.phantomjs())
                 .get(Configuration.browser)
                 .setup();
+
+        Configuration.browserCapabilities = Map.of("firefox", firefoxCapabilities())
+                .get(Configuration.browser);
         Configuration.timeout = 15000;
     }
 
@@ -51,7 +67,7 @@ public class DemoProjectListener implements ISuiteListener, IInvokedMethodListen
     }
 
     private void assignDocumentationToTest(IInvokedMethod method) {
-        if(!checkIfDocumentationAnnotationIsPresent(method)) {
+        if (!checkIfDocumentationAnnotationIsPresent(method)) {
             return;
         }
 
@@ -63,7 +79,7 @@ public class DemoProjectListener implements ISuiteListener, IInvokedMethodListen
                     Allure.getLifecycle().getCurrentTestCase().get(),
                     testResult -> {
                         try {
-                            testResult.setDescriptionHtml((String)documentation.invoke(annotatedTest.fromClass().newInstance()));
+                            testResult.setDescriptionHtml((String) documentation.invoke(annotatedTest.fromClass().newInstance()));
                         } catch (IllegalAccessException e) {
                             e.printStackTrace();
                         } catch (InvocationTargetException e) {
@@ -91,8 +107,8 @@ public class DemoProjectListener implements ISuiteListener, IInvokedMethodListen
                 .getAnnotation(DocumentedTest.class);
     }
 
-    private Method getDocumentationMethodFromAnnotation (DocumentedTest annotation) {
-        try{
+    private Method getDocumentationMethodFromAnnotation(DocumentedTest annotation) {
+        try {
             return annotation.fromClass().getDeclaredMethod(annotation.byMethod(), null);
         } catch (NoSuchMethodException e) {
             e.printStackTrace();
